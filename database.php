c4662d35f49dbe0e733ef1f0bfa4158c9f638518
Elliot Adderton <elliot@ellcom.net>
2013-11-22 17:20:57 +0000

    Changes to database connections

--- database.php
+++ database.php
@@ -3,8 +3,25 @@
 class Database extends PDO {
 
 	function __construct() {
-    	parent::__construct('mysql:dbname=match;host=localhost', getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"));
-    	$this->setAttribute(PDO::ATTR_STATEMENT_CLASS, array('DBStatement', array($this)));
+    	try {
+    		parent::__construct('mysql:dbname=match;host=localhost', getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"));
+    		$this->setAttribute(PDO::ATTR_STATEMENT_CLASS, array('DBStatement', array($this)));
+		} catch (PDOException $e) {
+			throw $e;
+		}
+	}
+	
+	function authenticationCredentials($user, $password) {
+		$statement = $this->prepare("SELECT * FROM users WHERE username = :username AND password = :password LIMIT 1");
+		$statement->bindValue(':username',$user);
+		$hashedPassword = hash("sha256","^7~Q?zÉ".$password);
+		$statement->bindValue(':password', $hashedPassword);
+		$statement->execute();
+		if($statement->rowCount() == 0){
+			return false;
+		}else{
+			return $statement->fetch(PDO::FETCH_ASSOC);
+		}
 	}
 }
 
