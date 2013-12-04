a1304f08c5d33f4e0fb88a7c711d4f20d1a1685e
benthurlow <benthurlow@hotmail.co.uk>
2013-12-04 13:59:31 +0000

    adding functionality added

--- question.php
+++ question.php
@@ -19,4 +19,35 @@ foreach ($questionArray as $row)
 echo '<input type="submit" value="Submit">';
 echo '</form>';
 
+
+
+
+// ===== ADDING TO THE DB ======
+// adding new candidate answers to the database 
+// check opinion has been set and is not empty.
+foreach ($questionArray as $row) 
+{
+	$questionID = $row['id'];
+
+	if (isset($_GET['A'.$questionID.'']) && !empty($_GET['A'.$questionID.'']))
+	{
+		// retrieve the value from the HTML radio button 'opinion'
+		$input = $_GET['A'.$questionID.''];
+		
+		// check opinion parameter within bounds (stop some injection)
+		if($input > 0 && $input <= 5)
+		{
+			echo ''.$questionID.' Radio button '.$input.' selected.<br>';
+			
+			// candidate id should be obtained from session/login
+			// 1 IS HERE AS A PLACEHOLDER
+			$database->insertAnswer($input, $questionID, 1);
+		}
+		else
+		{
+			echo 'ERROR (testAdd): incorrect opinion parameter<br>';
+		}
+	}
+}
+
 ?>
