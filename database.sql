6f0a58652c1e23272803cdd7956da521fad538e2
Elliot Adderton <elliot@ellcom.net>
2013-12-05 18:07:07 +0000

    User Pictures

--- database.sql
+++ database.sql
@@ -1,34 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.33, for osx10.6 (i386)
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 05, 2013 at 05:43 PM
--- Server version: 5.5.32
--- PHP Version: 5.4.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: match
+-- ------------------------------------------------------
+-- Server version	5.5.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `match`
---
-CREATE DATABASE IF NOT EXISTS `match` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `match`;
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `candidateanswers`
 --
 
-CREATE TABLE IF NOT EXISTS `candidateanswers` (
+DROP TABLE IF EXISTS `candidateanswers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `candidateanswers` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `questionID` int(11) unsigned NOT NULL,
   `candidateID` int(11) unsigned NOT NULL,
@@ -36,46 +30,30 @@ CREATE TABLE IF NOT EXISTS `candidateanswers` (
   `justification` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `questionID` (`questionID`),
-  KEY `candidateID` (`candidateID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
+  KEY `candidateID` (`candidateID`),
+  CONSTRAINT `candidateanswers_ibfk_1` FOREIGN KEY (`questionID`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `candidateanswers_ibfk_2` FOREIGN KEY (`candidateID`) REFERENCES `candidates` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `candidateanswers`
 --
 
-INSERT INTO `candidateanswers` (`id`, `questionID`, `candidateID`, `answer`, `justification`) VALUES
-(21, 1, 1, 1, NULL),
-(22, 2, 1, 5, NULL),
-(23, 3, 1, 4, NULL),
-(24, 4, 1, 2, NULL),
-(25, 5, 1, 5, NULL),
-(26, 6, 1, 3, NULL),
-(27, 7, 1, 3, NULL),
-(28, 8, 1, 3, NULL),
-(29, 9, 1, 3, NULL),
-(30, 10, 1, 3, NULL),
-(31, 1, 2, 1, NULL),
-(32, 2, 2, 2, NULL),
-(33, 3, 2, 3, NULL),
-(34, 4, 2, 4, NULL),
-(35, 5, 2, 5, NULL),
-(36, 6, 2, 5, NULL),
-(37, 7, 2, 4, NULL),
-(38, 8, 2, 3, NULL),
-(39, 9, 2, 2, NULL),
-(40, 10, 2, 1, NULL),
-(41, 11, 1, 3, NULL),
-(42, 12, 1, 3, NULL),
-(43, 11, 2, 2, NULL),
-(44, 12, 2, 5, NULL);
-
--- --------------------------------------------------------
+LOCK TABLES `candidateanswers` WRITE;
+/*!40000 ALTER TABLE `candidateanswers` DISABLE KEYS */;
+INSERT INTO `candidateanswers` VALUES (21,1,1,1,NULL),(22,2,1,5,NULL),(23,3,1,4,NULL),(24,4,1,2,NULL),(25,5,1,5,NULL),(26,6,1,3,NULL),(27,7,1,3,NULL),(28,8,1,3,NULL),(29,9,1,3,NULL),(30,10,1,3,NULL),(31,1,2,1,NULL),(32,2,2,2,NULL),(33,3,2,3,NULL),(34,4,2,4,NULL),(35,5,2,5,NULL),(36,6,2,5,NULL),(37,7,2,4,NULL),(38,8,2,3,NULL),(39,9,2,2,NULL),(40,10,2,1,NULL),(41,11,1,3,NULL),(42,12,1,3,NULL),(43,11,2,2,NULL),(44,12,2,5,NULL);
+/*!40000 ALTER TABLE `candidateanswers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `candidates`
 --
 
-CREATE TABLE IF NOT EXISTS `candidates` (
+DROP TABLE IF EXISTS `candidates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `candidates` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `userID` int(11) unsigned DEFAULT NULL,
   `electionID` int(11) unsigned DEFAULT NULL,
@@ -87,122 +65,137 @@ CREATE TABLE IF NOT EXISTS `candidates` (
   `recomendationCount` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `userID` (`userID`),
-  KEY `electionID` (`electionID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  KEY `electionID` (`electionID`),
+  CONSTRAINT `candidates_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `candidates_ibfk_2` FOREIGN KEY (`electionID`) REFERENCES `elections` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `candidates`
 --
 
-INSERT INTO `candidates` (`id`, `userID`, `electionID`, `age`, `gender`, `course`, `picture`, `manifestoLink`, `recomendationCount`) VALUES
-(1, 2, 1, 21, 'M', 'Computer Science', 'me.png', 'mymanifesto.html', NULL),
-(2, 3, 1, 24, 'M', 'Women''s studies', 'test.png', 'testmanifesto.org', NULL);
-
--- --------------------------------------------------------
+LOCK TABLES `candidates` WRITE;
+/*!40000 ALTER TABLE `candidates` DISABLE KEYS */;
+INSERT INTO `candidates` VALUES (1,2,1,21,'M','Computer Science','me.png','mymanifesto.html',NULL),(2,3,1,24,'M','Women\'s studies','test.png','testmanifesto.org',NULL);
+/*!40000 ALTER TABLE `candidates` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `electionquestions`
 --
 
-CREATE TABLE IF NOT EXISTS `electionquestions` (
+DROP TABLE IF EXISTS `electionquestions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `electionquestions` (
   `electionID` int(11) unsigned NOT NULL,
   `questionID` int(11) unsigned NOT NULL,
   KEY `electionID` (`electionID`),
-  KEY `questionID` (`questionID`)
+  KEY `questionID` (`questionID`),
+  CONSTRAINT `electionquestions_ibfk_1` FOREIGN KEY (`electionID`) REFERENCES `elections` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `electionquestions_ibfk_2` FOREIGN KEY (`questionID`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `electionquestions`
 --
 
-INSERT INTO `electionquestions` (`electionID`, `questionID`) VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(1, 4),
-(1, 5),
-(1, 6),
-(1, 7),
-(1, 8),
-(1, 9),
-(1, 10);
-
--- --------------------------------------------------------
+LOCK TABLES `electionquestions` WRITE;
+/*!40000 ALTER TABLE `electionquestions` DISABLE KEYS */;
+INSERT INTO `electionquestions` VALUES (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(1,10);
+/*!40000 ALTER TABLE `electionquestions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `elections`
 --
 
-CREATE TABLE IF NOT EXISTS `elections` (
+DROP TABLE IF EXISTS `elections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `elections` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(200) DEFAULT NULL,
   `timestamp` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `elections`
 --
 
-INSERT INTO `elections` (`id`, `name`, `timestamp`) VALUES
-(1, 'First Election', 1385911631);
-
--- --------------------------------------------------------
+LOCK TABLES `elections` WRITE;
+/*!40000 ALTER TABLE `elections` DISABLE KEYS */;
+INSERT INTO `elections` VALUES (1,'First Election',1385911631);
+/*!40000 ALTER TABLE `elections` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `questions`
 --
 
-CREATE TABLE IF NOT EXISTS `questions` (
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `questionText` varchar(200) NOT NULL DEFAULT '',
   `category` int(11) DEFAULT NULL,
   `divisiveness` float DEFAULT NULL,
   `selected` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `questions`
 --
 
-INSERT INTO `questions` (`id`, `questionText`, `category`, `divisiveness`, `selected`) VALUES
-(1, 'Should we kill 3 billion people to save Earth?', NULL, 4, 0),
-(2, 'Should Ben cut his hair?', NULL, 0.612372, 0),
-(3, 'Is Garlen the coolest guy you know?', NULL, 0.612372, 0),
-(4, 'Does Elliot often say the gayest thing of the day?', NULL, 0, 0),
-(5, 'Are pears a tasty fruit?', NULL, 4, 0),
-(6, 'Stack Overflow is the most useful website', NULL, 1.22474, 0),
-(7, 'Bing is a superior search engine to Google', NULL, 0.612372, 0),
-(8, 'Elliot is more strawberry blonde than ginger ', NULL, 4, 0),
-(9, 'Garlen is too tall for his own good', NULL, 0.612372, 0),
-(10, 'Tesco is Garlens favourite shop in the world', NULL, 1.22474, 0),
-(11, 'There should be more questions in this DB', NULL, 0.612372, 0),
-(12, 'To what extent do you agree with this statement', NULL, 1.22474, 0);
-
--- --------------------------------------------------------
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'Should we kill 3 billion people to save Earth?',NULL,4,0),(2,'Should Ben cut his hair?',NULL,0.612372,0),(3,'Is Garlen the coolest guy you know?',NULL,0.612372,0),(4,'Does Elliot often say the gayest thing of the day?',NULL,0,0),(5,'Are pears a tasty fruit?',NULL,4,0),(6,'Stack Overflow is the most useful website',NULL,1.22474,0),(7,'Bing is a superior search engine to Google',NULL,0.612372,0),(8,'Elliot is more strawberry blonde than ginger ',NULL,4,0),(9,'Garlen is too tall for his own good',NULL,0.612372,0),(10,'Tesco is Garlens favourite shop in the world',NULL,1.22474,0),(11,'There should be more questions in this DB',NULL,0.612372,0),(12,'To what extent do you agree with this statement',NULL,1.22474,0);
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `sessions`
 --
 
-CREATE TABLE IF NOT EXISTS `sessions` (
+DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sessions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `userID` int(11) unsigned NOT NULL,
   `sessionID` varchar(40) NOT NULL DEFAULT '',
   `timestamp` int(11) DEFAULT NULL,
   `lastSeen` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `userID` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `userID` (`userID`),
+  CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `sessions`
+--
+
+LOCK TABLES `sessions` WRITE;
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL DEFAULT '',
   `password` varchar(64) NOT NULL DEFAULT '',
@@ -210,97 +203,81 @@ CREATE TABLE IF NOT EXISTS `users` (
   `active` tinyint(1) NOT NULL DEFAULT '1',
   `email` varchar(100) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
+  `picture` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
-  KEY `type` (`type`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  KEY `type` (`type`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`type`) REFERENCES `usertypes` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `type`, `active`, `email`, `name`) VALUES
-(1, 'admin', '8eedf5e5ff3df74b923c545df2e0af1472d8245bc46ff24298774b72cd0a043b', 'admin', 1, 'admin@localhost', NULL),
-(2, 'BenThurlow', 'pass123', 'candidate', 1, 'ben@localhost.co.uk', 'Ben'),
-(3, 'JoeBloggs', 'pass456', 'candidate', 1, 'joebloggs@localhost.co.uk', 'Joe');
-
--- --------------------------------------------------------
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','8eedf5e5ff3df74b923c545df2e0af1472d8245bc46ff24298774b72cd0a043b','admin',1,'admin@localhost',NULL,NULL),(2,'BenThurlow','pass123','candidate',1,'ben@localhost.co.uk','Ben',NULL),(3,'JoeBloggs','pass456','candidate',1,'joebloggs@localhost.co.uk','Joe',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `usertypes`
 --
 
-CREATE TABLE IF NOT EXISTS `usertypes` (
+DROP TABLE IF EXISTS `usertypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usertypes` (
   `name` varchar(10) NOT NULL DEFAULT '',
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `usertypes`
 --
 
-INSERT INTO `usertypes` (`name`) VALUES
-('admin'),
-('candidate');
-
--- --------------------------------------------------------
+LOCK TABLES `usertypes` WRITE;
+/*!40000 ALTER TABLE `usertypes` DISABLE KEYS */;
+INSERT INTO `usertypes` VALUES ('admin'),('candidate');
+/*!40000 ALTER TABLE `usertypes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `voteranswers`
 --
 
-CREATE TABLE IF NOT EXISTS `voteranswers` (
+DROP TABLE IF EXISTS `voteranswers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `voteranswers` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `questionID` int(11) unsigned NOT NULL,
   `answer` int(1) unsigned NOT NULL,
   `count` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `questionID` (`questionID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `questionID` (`questionID`),
+  CONSTRAINT `voteranswers_ibfk_1` FOREIGN KEY (`questionID`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Constraints for dumped tables
+-- Dumping data for table `voteranswers`
 --
 
---
--- Constraints for table `candidateanswers`
---
-ALTER TABLE `candidateanswers`
-  ADD CONSTRAINT `candidateanswers_ibfk_1` FOREIGN KEY (`questionID`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `candidateanswers_ibfk_2` FOREIGN KEY (`candidateID`) REFERENCES `candidates` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+LOCK TABLES `voteranswers` WRITE;
+/*!40000 ALTER TABLE `voteranswers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `voteranswers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Constraints for table `candidates`
---
-ALTER TABLE `candidates`
-  ADD CONSTRAINT `candidates_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `candidates_ibfk_2` FOREIGN KEY (`electionID`) REFERENCES `elections` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `electionquestions`
---
-ALTER TABLE `electionquestions`
-  ADD CONSTRAINT `electionquestions_ibfk_1` FOREIGN KEY (`electionID`) REFERENCES `elections` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `electionquestions_ibfk_2` FOREIGN KEY (`questionID`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `sessions`
---
-ALTER TABLE `sessions`
-  ADD CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`type`) REFERENCES `usertypes` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `voteranswers`
---
-ALTER TABLE `voteranswers`
-  ADD CONSTRAINT `voteranswers_ibfk_1` FOREIGN KEY (`questionID`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-12-05 18:06:14
