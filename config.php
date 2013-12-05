5664df339a2852c28b600ffb49037ef7b419be64
Elliot Adderton <elliot@ellcom.net>
2013-12-05 16:38:37 +0000

    Tidy up

--- config.php
+++ config.php
@@ -9,22 +9,22 @@ if (strpos($_SERVER["REQUEST_URI"], 'config.php') !== FALSE){
 }
 
 // Change to your apache directory for the site
-$ROOT_FOLDER = "/Users/elliot/Sites/Candidate-Match";
+define("ROOT_FOLDER","/Users/elliot/Sites/Candidate-Match");
 // Change to your Smarty folder
-$SMARTY_FOLDER = "/Users/elliot/Sites/Smarty-3.1.15";
+define("SMARTY_FOLDER", "/Users/elliot/Sites/Smarty-3.1.15");
 
 // Database
 // This should be set as a sysenv var when pushed to the deployment server
 putenv("MYSQL_USER=root");
 putenv("MYSQL_PASSWORD=root");
-require_once($ROOT_FOLDER."/database.php");
+require_once(ROOT_FOLDER."/database.php");
 $database = new Database;
 
 // Smarty Config
-$TEMPLATE_FOLDER = $ROOT_FOLDER."/smarty_tpl";
-require_once($SMARTY_FOLDER."/libs/Smarty.class.php");
+define("TEMPLATE_FOLDER",ROOT_FOLDER."/smarty_tpl");
+require_once(SMARTY_FOLDER."/libs/Smarty.class.php");
 $smarty = new Smarty;
-$smarty->setTemplateDir($TEMPLATE_FOLDER);
+$smarty->setTemplateDir(TEMPLATE_FOLDER);
 $smarty->debugging = $DEBUG_MODE; 
 
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 require_once('config.php');
+
+//$smarty->assign("myvar","John");
 $smarty->display('index.tpl');
 
 ?>
