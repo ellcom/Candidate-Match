8f0a4d44050e943ae8c932429af4efe86a29b60b
Elliot Adderton <elliot@ellcom.net>
2013-12-10 18:47:25 +0000

    Cookie Monster Fix 2

--- session.php
+++ session.php
@@ -45,6 +45,15 @@ class Session {
 			$database->updateSession($sessionID,$_SERVER['REQUEST_URI']);
 			return true;
 		}else {
+			if (isset($_SERVER['HTTP_COOKIE'])) {
+			    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+			    foreach($cookies as $cookie) {
+			        $parts = explode('=', $cookie);
+			        $name = trim($parts[0]);
+			        setcookie($name, '', time()-1000);
+			        setcookie($name, '', time()-1000, '/');
+			    }
+			}
 			return false;
 		}
 	}
