4b024ff9ed786dde02caf1e6d4856a2a3a665d2c
benermafield <andymania@gmail.com>
2013-12-11 22:22:40 +0000

    Graphs Complete
    Voters.php functionality done, but it does need some code tidying and
    test echos removed. There are 3 graphs there atm, we can find a way to
    only show one at a time, and let the user choose what type they want to
    see, or whatever, or just choose one graph ourselves and just use that.

--- smarty_tpl/results.tpl
+++ smarty_tpl/results.tpl
@@ -2,10 +2,14 @@
 
 <script src="SpryAssets/SpryCollapsiblePanel.js" type="text/javascript"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/Chart.js/0.2.0/Chart.min.js" type="text/javascript"></script>
+<script src="scripts/resultsGraph.js" type="text/javascript"></script>
 <link href="SpryAssets/SpryCollapsiblePanel.css" rel="stylesheet" type="text/css" />
 
 <section id="main-matter">
 	<h1>Results Page</h1>
+	<canvas id="graph1" height="400" width="400"></canvas>
+	<canvas id="graph2" height="400" width="400"></canvas>
+	<canvas id="graph3" height="400" width="400"></canvas>
 </section>
 
 {include file="footer.tpl"}
--- voters.php
+++ voters.php
@@ -26,8 +26,54 @@ if(isset($_POST['submit']))
 	$voterSimilarities = $matchObj->calculateDifferences();
 
 	print_r($voterSimilarities);
-
+	
+	// change to simple arrays
+	$graphNamesPHP = array();
+	$graphValsPHP = array();
+	foreach($voterSimilarities as $row)
+	{
+		$candidateInfo = $database->returnDataForCandidate($electionID['0']['id'], $row['candidateID']);
+		print_r($candidateInfo);
+		array_push($graphNamesPHP, $candidateInfo['name']);
+		array_push($graphValsPHP, $row['similarity']);
+	}
+	print_r($graphNamesPHP);
+	print_r($graphValsPHP);
+	
+	// convert to JSON arrays
+	$graphNamesJS = json_encode($graphNamesPHP);
+	$graphValsJS = json_encode($graphValsPHP);
+	
 	$smarty->display('results.tpl');
+	
+	// generate graph
+	?>
+	<script>
+		var data =
+		{
+			labels : <?php echo $graphNamesJS; ?>,
+			datasets : [
+			{
+				fillColor : "rgba(220,220,220,0.5)",
+				strokeColor : "rgba(220,220,220,1)",
+				pointColor : "rgba(220,220,220,1)",
+				pointStrokeColor : "#fff",
+				data : <?php echo $graphValsJS ?>
+			}
+			]
+		}
+		var ctx1 = document.getElementById("graph1").getContext("2d");
+		var jGraph1 = new Chart(ctx1).Bar(data);
+		
+		var ctx2 = document.getElementById("graph2").getContext("2d");
+		var jGraph2 = new Chart(ctx2).Radar(data);
+		
+		var ctx3 = document.getElementById("graph3").getContext("2d");
+		var jGraph3 = new Chart(ctx3).Line(data);
+	</script>
+	<?php
+
+	
 }
 else
 {
