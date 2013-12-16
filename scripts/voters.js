de22676d22f167e78c0d4f338342c13bb3e1bf71
benermafield <andymania@gmail.com>
2013-12-16 22:02:19 +0000

    Update voters.js

--- scripts/voters.js
+++ scripts/voters.js
@@ -4,16 +4,16 @@ $(document).ready(function() {
 	
 	var names = {};
 	$('input:radio').each(function() {
-		  names[$(this).attr('name')] = true;
+		names[$(this).attr('name')] = true;
 	});
 	
 	$('input:radio').change(function() {
-	//	var count = 0;
-	//	$.each(names, function() {
-	//		  count++;
-	//	});
+		var count = 0;
+		$.each(names, function() {
+			count++;
+		});
 		
-		if($('input:radio:checked').length == names.length) {
+		if($('input:radio:checked').length == count) {
 			$('input[type="submit"]').removeAttr('disabled');
 		}
 	})
