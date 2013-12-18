3d13e52412ce6f21ec5dac2dd01738b3a792fb83
benthurlow <benthurlow@hotmail.co.uk>
2013-12-18 13:54:56 +0000

    simplified insertanswer

--- database.php
+++ database.php
@@ -405,19 +405,13 @@ class Database extends PDO {
 	// is no question answer for that candidate.
 	private function checkForAnswer($questionID, $candidateID) 
 	{
-		$check_query = $this->prepare("SELECT candidateID FROM candidateanswers WHERE questionID = :questionID AND candidateID = :candidateID");
+		$check_query = $this->prepare("SELECT * FROM candidateanswers WHERE questionID = :questionID AND candidateID = :candidateID");
 		$check_query->bindParam(':questionID', $questionID);
 		$check_query->bindParam(':candidateID', $candidateID);
 
 		$check_query->execute();
-		$check_query->fetchAll();
 
-		if(sizeof($check_query) == 0)
-		{
-			$check_query = NULL;
-		}
-
-		return $check_query;
+		return $check_query->rowCount();
 	}
 	// ==================================================
 	// END FUNCTION checkForAnswer
@@ -432,12 +426,16 @@ class Database extends PDO {
 	{
 		$present = $this->checkForAnswer($questionID, $candidateID);
 
-		if($present != NULL) // answer already exists, so update it
+		//echo 'p:'.$present;
+
+		if($present != 0) // answer already exists, so update it
 		{
+			//echo "already exists";
 			$this->updateAnswer($answer, $justification, $questionID, $candidateID);
 		}
 		else // answer doesnt exist, so add it.
 		{
+			//echo "adding";
 			$this->addAnswer($answer, $justification, $questionID, $candidateID);
 		}
 	}
