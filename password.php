99e13031f8fef692758200709374dac4ae05bc07
Elliot Adderton <elliot@ellcom.net>
2013-12-18 11:31:03 +0000

    Change Password

--- password.php
+++ password.php
@@ -12,8 +12,8 @@ if($session->checkSession()){
   	} elseif (!preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9A-Za-z!@#$%]{8,40}$/',$_POST['password1'])) {
   		$smarty->assign("message","New Passwords must be 8 charactors (40 max) in length, contain 1 digit, 1 upercase and 1 lowercase letter");
   	}else{
-  		if($database->changeUserPassword($_SESSION['username'],$_POST['password0'],$_POST['password1'])){
-  			header("Location: ./dashboard.php",true, 301);
+  		if($database->changeMyPassword($_SESSION['id'],$_POST['password0'],$_POST['password1'])){
+  			header("Location: ./dashboard.php");
   		} else {
   			$smarty->assign("message","Invalid Password");
   		}
